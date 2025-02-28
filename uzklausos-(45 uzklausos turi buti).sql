34602538b2230638397f6a03ae8de18df91b3284
Gabrielė <134537965+gabwowce@users.noreply.github.com>
2025-02-28 14:09:32 +0200

    Add files via upload

--- uzklausos-(45 uzklausos turi buti).sql
+++ uzklausos-(45 uzklausos turi buti).sql
@@ -1,4 +1,4 @@
--- Įterpiamos sąskaitos tik užbaigtiems arba patvirtintiems užsakymams
+-- 1. Įterpiamos sąskaitos tik užbaigtiems arba patvirtintiems užsakymams
 INSERT INTO `Saskaitos` (`uzsakymo_id`, `suma`, `saskaitos_data`)
 SELECT 
     `uzsakymo_id`, 
@@ -8,7 +8,7 @@ FROM `Uzsakymai`
 WHERE `uzsakymo_busena` IN ('užbaigta', 'patvirtinta');
 
 
--- Įterpiami mokėjimai tik toms sąskaitoms, kurios buvo sukurtos
+-- 2. Įterpiami mokėjimai tik toms sąskaitoms, kurios buvo sukurtos
 INSERT INTO `Mokejimai` (`uzsakymo_id`, `suma`, `mokejimo_data`, `mokejimo_tipas`)
 SELECT 
     `uzsakymo_id`, 
@@ -22,3 +22,31 @@ SELECT
     END AS `mokejimo_tipas`
 FROM `Saskaitos`;
 
+-- 3. Kiek klientų yra užsakę bent vieną automobilį?
+SELECT COUNT(DISTINCT kliento_id) AS uzsake_klientai
+FROM Uzsakymai;
+
+-- 4. Kokie klientai išleido daugiausiai pinigų nuomai?
+SELECT k.kliento_id, k.vardas, k.pavarde, SUM(u.bendra_kaina) AS viso_isleista
+FROM Uzsakymai u
+JOIN Klientai k ON u.kliento_id = k.kliento_id
+GROUP BY k.kliento_id
+ORDER BY viso_isleista DESC
+LIMIT 10;
+
+-- 5. Kokie klientai dažniausiai naudojasi nuolaidomis?
+SELECT k.kliento_id, k.vardas, k.pavarde, COUNT(un.uzsakymo_nuolaidos_id) AS nuolaidu_kiekis
+FROM Uzsakymo_Nuolaidos un
+JOIN Uzsakymai u ON un.uzsakymo_id = u.uzsakymo_id
+JOIN Klientai k ON u.kliento_id = k.kliento_id
+GROUP BY k.kliento_id
+ORDER BY nuolaidu_kiekis DESC
+LIMIT 10;
+
+-- 6. Klientų, kurie turi nesumokėtų sąskaitų, sąrašas
+SELECT k.kliento_id, k.vardas, k.pavarde, s.saskaitos_id, s.suma
+FROM Saskaitos s
+JOIN Uzsakymai u ON s.uzsakymo_id = u.uzsakymo_id
+JOIN Klientai k ON u.kliento_id = k.kliento_id
+LEFT JOIN Mokejimai m ON s.saskaitos_id = m.uzsakymo_id
+WHERE m.mokejimo_id IS NULL;
